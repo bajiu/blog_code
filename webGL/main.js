949e00feca9dc6f4a2bbf4303d6a645be49574c3
bajiu <bajiu@meishubao.com>
2021-05-13 22:03:44 +0800

    暂存

--- webGL/main.js
+++ webGL/main.js
@@ -3,9 +3,10 @@
 
 
 /**
- *  2021/05/12
+ *  2021/05/12 - 2021/05/13
  *  webGl 坐标系统
  *  attribute 变量
+ *  uniform 变量
  */
 
 
@@ -28,6 +29,7 @@ const draw_attribute = () => {
         // attribute vec4 a_Position;
         
         attribute vec4 a_Position;
+        
         void main() {
             gl_Position = a_Position;
             gl_PointSize = 10.0;
@@ -35,9 +37,14 @@ const draw_attribute = () => {
     `;
     const FSHADER_SOCUCE =
         `
+        
+        
+        precision mediump float;
+        uniform vec4 u_FragColor;
+        
         void main() {
             // 设置颜色
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            gl_FragColor = u_FragColor;
         }
     `;
 
@@ -45,6 +52,9 @@ const draw_attribute = () => {
         console.error('你指定是哪儿有点儿毛病');
         return;
     }
+
+    // ### **获取 a_Position 变量的储存位置**
+
 
     // gl.getAttribLocation(program, name)  获取 attribute 变量地址
     // 返回值是 attribute 的储存地址
@@ -80,29 +90,75 @@ const draw_attribute = () => {
     // gl.vertexAttribute4fv(a_Position, position)
 
 
-    gl.clear(gl.COLOR_BUFFER_BIT)
+    // ### **获取 u_FragColor 变量的储存位置**
+    // 具体地址信息上面有介绍
+    const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (u_FragColor < 0) {
+        console.error('u_FragColor 有点儿毛病');
+        return;
+    }
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+
+    // gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // gl.drawArrays(gl.POINTS, 0, 1);
 
 
     // 坐标数组
     const g_points = [];
+    // 颜色数组
+    const g_colors = [];
     // 点击事件
-    const click = (event, gl, canvas) => {
+    const click = (event, gl, canvas, a_Position, u_FragColor) => {
         const x = event.clientX; // X坐标
         const y = event.clientY; // Y坐标
         const rect = event.target.getBoundingClientRect();
+        const positionX = (x - rect.left - canvas.height / 2) / (canvas.height / 2);
+        const positionY = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+        // 将坐标储存到 g_points 数组中
+        g_points.push(positionX);
+        g_points.push(positionY);
 
+
+        // 将颜色储存到 g_color 数组中
+        if (positionX >= 0.0 && positionY >= 0.0) {
+            // 第一象限
+            g_colors.push([1.0, 0.0, 0.0, 1.0]); // 红色
+        } else if (positionX < 0.0 && positionY < 0.0) {
+            // 第三象限
+            g_colors.push([0.0, 1.0, 0.0, 1.0]); // 绿色
+        } else {
+            // 另外两个
+            g_colors.push([1.0, 1.0, 1.0, 1.0]); // 白色
+        }
+
+        // 清除canvas (因为在绘制点之后, 颜色缓冲区就被 webGl 重置为了默认颜色(0.0, 0.0, 0.0, 0.0) ,默认alpha 的分量是 0.0 所以给整透明了)
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        const len = g_points.length;
+        for (let i = 0; i < len; i += 2) {
+            const rgba = g_colors[i/2];
+            // 将点的位置传递到变量中 a_Position
+            gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+            console.log(rgba)
+
+            gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+
+            gl.drawArrays(gl.POINTS, 0, 1);
+
+        }
 
     }
     canvas.onmousedown = (event) => {
-
-        click(event, gl, canvas, a_Position);
+        click(event, gl, canvas, a_Position, u_FragColor);
 
     }
 }
-
 draw_attribute()
+
+// 不知道有没有用
+//早期软件渲染的时代，图形绘制的确是会去判断当前界面哪些像素区域可以复用，然后优先绘制这部分内容，下一次绘制的时候就不必清空它了，这样就能达到优化的目的，但是这样的优化会导致一些游戏中常见的著名的“镜子大厅Hall of mirrors”的效应。在现代的图形绘制 这种优化已经不再有效了，最新的gpu以不同的方式工作，如果屏幕是清空状态，反而能工作的更快，**通过gpu清空屏幕 ，可以节省帧拷贝浪费的时间。因为gpu的工作方式，清空屏幕可以帮助避免很多问题，如 闪烁，或者有物品没有绘制。**
 
 
 
