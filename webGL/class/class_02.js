3e0c4bacd2142899f943778c80863d3376a134d4
bajiu <bajiu0819@163.com>
2021-05-14 00:37:38 +0800

    巴啦啦学习能量

---
+++ webGL/class/class_02.js
@@ -0,0 +1,175 @@
+// 齐次坐标(x,y,z,w) 等价于三维坐标 (x/w, y/w, z/w)
+// 如果齐次坐标的第四个分量是 1 就可以当三维坐标使用, w 的值必须是大于等于 0 的 如果趋近于0, 那么表示无穷远, 所以在齐次坐标系中是有无穷概念的
+
+
+/**
+ *  2021/05/12 - 2021/05/13
+ *  webGl 坐标系统
+ *  attribute 变量
+ *  uniform 变量
+ */
+
+
+// webGl 坐标系统
+// webgl 右手坐标系 观察点位于 (0.0, 0.0, 0.0) 视线是 Z轴的负方向
+
+// webgl 和 canvas 的坐标关系:
+// - `<canvas>` 的中心点: (0.0, 0.0, 0.0)
+// - `<canvas>` 的上边缘和下边缘: (-1.0, 0.0, 0.0) 和 (1.0, 0.0, 0.0)
+// - `<canvas>` 的左边缘和右边缘: (0.0, -1.0, 0.0) 和 (0.0, 1.0, 0.0)
+
+// attribute 变量
+
+
+const draw_attribute = () => {
+    const VSHADER_SOURCE =
+        `
+        // 储备限定符  类型    变量名
+        //    ｜      ｜      ｜
+        // attribute vec4 a_Position;
+        
+        attribute vec4 a_Position;
+        
+        void main() {
+            gl_Position = a_Position;
+            gl_PointSize = 10.0;
+        }
+    `;
+    const FSHADER_SOCUCE =
+        `
+        
+        
+        precision mediump float;
+        uniform vec4 u_FragColor;
+        
+        void main() {
+            // 设置颜色
+            gl_FragColor = u_FragColor;
+        }
+    `;
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOCUCE)) {
+        console.error('你指定是哪儿有点儿毛病');
+        return;
+    }
+
+    // ### **获取 a_Position 变量的储存位置**
+
+
+    // gl.getAttribLocation(program, name)  获取 attribute 变量地址
+    // 返回值是 attribute 的储存地址
+    // 错误信息:
+    // - `INVALID_OPERATION` (程序对象未能成功链接)
+    // - `INVALID_VALUE` (name 参数的长度大于 attribute 变量名默认长度, 默认256字节)
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.error('a_Position 有点儿毛病');
+        return;
+    }
+    // 将指定位置传递个 attribute 变量
+    gl.vertexAttrib3f(a_Position, 0.1, 0.1, 0.0);
+    // gl.vertexAttrib3f(location, v0, v1, v2)
+    // location 制定修改的 attribute 变量的储存位置
+    // v0, v1, v2 分别为 attribute 变量对应三个分量的值
+    // 错误信息:
+    // - `INVALID_OPERATION` 没有当前程序对象
+    // - `INVALID_VALUE` (location 大于等于 attribute 变量的最大数目 默认256字节)
+
+
+    // gl.vertexAttrib3f() 同族函数
+    // 该系列的函数的任务就是从 JavaScript 向顶点着色器中的 attribute 变量传值(v1, v2 默认传0.0 , v3默认 1.0)
+    // gl.vertexAttrib1f(location, v0)
+    // gl.vertexAttrib2f(location, v0, v1)
+    // gl.vertexAttrib3f(location, v0, v1, v2)
+    // gl.vertexAttrib4f(location, v0, v1, v2, v3)
+
+
+    // gl.vertexAttrib`(基础函数名)`3`(参数个数)`f`(参数类型: 'f' 表示浮点数, 'i' 表示整数)`
+
+    // const position = new Float32Array([1.0, 2.0, 3.0, 1.0])
+    // gl.vertexAttribute4fv(a_Position, position)
+
+
+    // ### **获取 u_FragColor 变量的储存位置**
+    // 具体地址信息上面有介绍
+    const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (u_FragColor < 0) {
+        console.error('u_FragColor 有点儿毛病');
+        return;
+    }
+
+
+    // gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // gl.drawArrays(gl.POINTS, 0, 1);
+
+
+    // 坐标数组
+    const g_points = [];
+    // 颜色数组
+    const g_colors = [];
+    // 点击事件
+    const click = (event, gl, canvas, a_Position, u_FragColor) => {
+        const x = event.clientX; // X坐标
+        const y = event.clientY; // Y坐标
+        const rect = event.target.getBoundingClientRect();
+        const positionX = (x - rect.left - canvas.height / 2) / (canvas.height / 2);
+        const positionY = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+        // 将坐标储存到 g_points 数组中
+        g_points.push(positionX);
+        g_points.push(positionY);
+
+
+        // 将颜色储存到 g_color 数组中
+        if (positionX >= 0.0 && positionY >= 0.0) {
+            // 第一象限
+            g_colors.push([1.0, 0.0, 0.0, 1.0]); // 红色
+        } else if (positionX < 0.0 && positionY < 0.0) {
+            // 第三象限
+            g_colors.push([0.0, 1.0, 0.0, 1.0]); // 绿色
+        } else {
+            // 另外两个
+            g_colors.push([1.0, 1.0, 1.0, 1.0]); // 白色
+        }
+
+        // 清除canvas (因为在绘制点之后, 颜色缓冲区就被 webGl 重置为了默认颜色(0.0, 0.0, 0.0, 0.0) ,默认alpha 的分量是 0.0 所以给整透明了)
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        const len = g_points.length;
+        for (let i = 0; i < len; i += 2) {
+            const rgba = g_colors[i/2];
+            // 将点的位置传递到变量中 a_Position
+            gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+            console.log(rgba)
+
+            gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+
+
+            gl.drawArrays(gl.POINTS, 0, 1);
+
+        }
+
+    }
+    canvas.onmousedown = (event) => {
+        click(event, gl, canvas, a_Position, u_FragColor);
+
+    }
+}
+draw_attribute()
+
+// 不知道有没有用
+//早期软件渲染的时代，图形绘制的确是会去判断当前界面哪些像素区域可以复用，然后优先绘制这部分内容，下一次绘制的时候就不必清空它了，这样就能达到优化的目的，但是这样的优化会导致一些游戏中常见的著名的“镜子大厅Hall of mirrors”的效应。在现代的图形绘制 这种优化已经不再有效了，最新的gpu以不同的方式工作，如果屏幕是清空状态，反而能工作的更快，**通过gpu清空屏幕 ，可以节省帧拷贝浪费的时间。因为gpu的工作方式，清空屏幕可以帮助避免很多问题，如 闪烁，或者有物品没有绘制。**
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- webGL/main.js
+++ webGL/main.js
@@ -1,175 +1,107 @@
-// 齐次坐标(x,y,z,w) 等价于三维坐标 (x/w, y/w, z/w)
-// 如果齐次坐标的第四个分量是 1 就可以当三维坐标使用, w 的值必须是大于等于 0 的 如果趋近于0, 那么表示无穷远, 所以在齐次坐标系中是有无穷概念的
-
-
 /**
- *  2021/05/12 - 2021/05/13
- *  webGl 坐标系统
- *  attribute 变量
- *  uniform 变量
+ *  2021/05/13
+ *  webGl 缓冲区对象
  */
 
 
-// webGl 坐标系统
-// webgl 右手坐标系 观察点位于 (0.0, 0.0, 0.0) 视线是 Z轴的负方向
+// webGL 提供了 **缓冲区对象(buffer object)**, 它可以一次性向着色器传入多个顶点的数据
+// 我们可以一次性像缓冲区对象中填入大量的顶点数据, 然后将这些数据保存在其中, 供顶点着色器使用
 
-// webgl 和 canvas 的坐标关系:
-// - `<canvas>` 的中心点: (0.0, 0.0, 0.0)
-// - `<canvas>` 的上边缘和下边缘: (-1.0, 0.0, 0.0) 和 (1.0, 0.0, 0.0)
-// - `<canvas>` 的左边缘和右边缘: (0.0, -1.0, 0.0) 和 (0.0, 1.0, 0.0)
+const buffer_object = () =>{
 
-// attribute 变量
-
-
-const draw_attribute = () => {
     const VSHADER_SOURCE =
         `
-        // 储备限定符  类型    变量名
-        //    ｜      ｜      ｜
-        // attribute vec4 a_Position;
-        
-        attribute vec4 a_Position;
-        
-        void main() {
-            gl_Position = a_Position;
-            gl_PointSize = 10.0;
-        }
-    `;
-    const FSHADER_SOCUCE =
+            attribute vec4 a_Position;
+            void main() {
+                gl_Position = a_Position;
+                gl_PointSize = 10.0;
+            }
         `
-        
-        
-        precision mediump float;
-        uniform vec4 u_FragColor;
-        
-        void main() {
-            // 设置颜色
-            gl_FragColor = u_FragColor;
-        }
-    `;
+    const FSHADER_SOURCE =
+        `
+         void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+         }   
+        `
 
-    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOCUCE)) {
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.error('你指定是哪儿有点儿毛病');
         return;
     }
 
-    // ### **获取 a_Position 变量的储存位置**
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // 设置顶点的位置
+    const initVertexBuffers = (gl) => {
+        const vertices = new Float32Array([
+            0.0, 0.5, -0.5, 0.5, -0.5
+        ]);
+        // 点的个数
+        const n = 3;
+
+        // 创建缓冲区对象
+        const vertexBuffer = gl.createBuffer();
+        // gl.deleteBuffer(buffer) 删除缓冲区对象
+
+        // 将缓冲区对象绑定到目标(在这里, 表示向顶点着色器提供传给 attribute 变量的数据)
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        // gl.bindBuffer(target, buffer); 把缓冲区对象 buffer 绑定到 target 上
+        // target 参数:
+        // - `gl.ArrayBuffer` 表示缓冲区包含顶点的数据
+        // - `gl.ELEMENT_ARRAY_BUFFER` 表示缓冲区对象中包含顶点的索引值(听说后面会学)
+        // buffer 参数: 指定之前的由 gl.createBuffer() 返回的待绑定的缓冲区对象
+
+        // 向缓冲区对象中写入数据
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+        // 将第二个参数 `vertices` 中的数据写入了绑定到 第一个参数 `gl.ARRAY_BUFFER` 上的缓冲区对象
+        // 我们不能直接向缓冲区写入数据, 只能向'目标'写入数据, 所以要向缓冲区写入数据, 必须先要绑定
+
+        // gl.bufferData(target, data, usage); 开辟存储空间, 向绑定在 target 上的缓冲区对象写入 data
+        // target gl.bindBuffer() 方法的一样
+        // usage 参数:
+        // - `gl.STATIC_DRAW`  只会向缓冲区对象写入一次数据, 但需要绘制很多次
+        // - `gl.STREAM_DRAW`  只会向缓冲区对象写入一次数据, 然后绘制若干次
+        // - `gl.DYNAMIC`      会向缓冲区对象中多次写入数据, 并绘制多次
 
 
-    // gl.getAttribLocation(program, name)  获取 attribute 变量地址
-    // 返回值是 attribute 的储存地址
-    // 错误信息:
-    // - `INVALID_OPERATION` (程序对象未能成功链接)
-    // - `INVALID_VALUE` (name 参数的长度大于 attribute 变量名默认长度, 默认256字节)
-    const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if (a_Position < 0) {
-        console.error('a_Position 有点儿毛病');
-        return;
-    }
-    // 将指定位置传递个 attribute 变量
-    gl.vertexAttrib3f(a_Position, 0.1, 0.1, 0.0);
-    // gl.vertexAttrib3f(location, v0, v1, v2)
-    // location 制定修改的 attribute 变量的储存位置
-    // v0, v1, v2 分别为 attribute 变量对应三个分量的值
-    // 错误信息:
-    // - `INVALID_OPERATION` 没有当前程序对象
-    // - `INVALID_VALUE` (location 大于等于 attribute 变量的最大数目 默认256字节)
+
+        const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+        // 将缓冲区对象分配给 a_Position 对象
+        gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+        // gl.vertexAttribPointer(location, size, type, normalized, stride, offset); 判定将 gl.ARRAY_BUFFER 的缓冲区对象分配给 location 指定的 attribute 变量
+        // type 参数:  gl.UNSIGNED_BYTE | gl.SHORT | gl.UNSIGNED_SHORT | gl.INT | gl.UNSIGNED_INT | gl.FLOAT
+        // normalized 参数: Boolean 值, 表明是否将非浮点型的数据归一化到 [0, 1] 或 [-1, 1] 之间
+        // stride 参数: 指定相邻两个顶点间的字节数, 默认为 0 (之后又会学到)
+        // offset 参数: 偏移量没什么好说的
 
 
-    // gl.vertexAttrib3f() 同族函数
-    // 该系列的函数的任务就是从 JavaScript 向顶点着色器中的 attribute 变量传值(v1, v2 默认传0.0 , v3默认 1.0)
-    // gl.vertexAttrib1f(location, v0)
-    // gl.vertexAttrib2f(location, v0, v1)
-    // gl.vertexAttrib3f(location, v0, v1, v2)
-    // gl.vertexAttrib4f(location, v0, v1, v2, v3)
 
 
-    // gl.vertexAttrib`(基础函数名)`3`(参数个数)`f`(参数类型: 'f' 表示浮点数, 'i' 表示整数)`
-
-    // const position = new Float32Array([1.0, 2.0, 3.0, 1.0])
-    // gl.vertexAttribute4fv(a_Position, position)
-
-
-    // ### **获取 u_FragColor 变量的储存位置**
-    // 具体地址信息上面有介绍
-    const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-    if (u_FragColor < 0) {
-        console.error('u_FragColor 有点儿毛病');
-        return;
-    }
+        // 连接 a_Position 变量与分配给它的缓冲区对象
+        gl.enableVertexAttribArray(a_Position);
+        // 为了使顶点着色器能够访问 缓冲区内的数据, 我们需要用 gl.enableVertexAttribArray(location) 方法来开启 attribute 变量
+        // `gl.disableVertexAttribArray(location)` 关闭分配
 
 
-    // gl.clear(gl.COLOR_BUFFER_BIT)
-
-    // gl.drawArrays(gl.POINTS, 0, 1);
-
-
-    // 坐标数组
-    const g_points = [];
-    // 颜色数组
-    const g_colors = [];
-    // 点击事件
-    const click = (event, gl, canvas, a_Position, u_FragColor) => {
-        const x = event.clientX; // X坐标
-        const y = event.clientY; // Y坐标
-        const rect = event.target.getBoundingClientRect();
-        const positionX = (x - rect.left - canvas.height / 2) / (canvas.height / 2);
-        const positionY = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
-        // 将坐标储存到 g_points 数组中
-        g_points.push(positionX);
-        g_points.push(positionY);
-
-
-        // 将颜色储存到 g_color 数组中
-        if (positionX >= 0.0 && positionY >= 0.0) {
-            // 第一象限
-            g_colors.push([1.0, 0.0, 0.0, 1.0]); // 红色
-        } else if (positionX < 0.0 && positionY < 0.0) {
-            // 第三象限
-            g_colors.push([0.0, 1.0, 0.0, 1.0]); // 绿色
-        } else {
-            // 另外两个
-            g_colors.push([1.0, 1.0, 1.0, 1.0]); // 白色
-        }
-
-        // 清除canvas (因为在绘制点之后, 颜色缓冲区就被 webGl 重置为了默认颜色(0.0, 0.0, 0.0, 0.0) ,默认alpha 的分量是 0.0 所以给整透明了)
-        gl.clear(gl.COLOR_BUFFER_BIT);
-
-        const len = g_points.length;
-        for (let i = 0; i < len; i += 2) {
-            const rgba = g_colors[i/2];
-            // 将点的位置传递到变量中 a_Position
-            gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
-            console.log(rgba)
-
-            gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-
-
-            gl.drawArrays(gl.POINTS, 0, 1);
-
-        }
+        return n;
 
     }
-    canvas.onmousedown = (event) => {
-        click(event, gl, canvas, a_Position, u_FragColor);
 
-    }
+
+    const n = initVertexBuffers(gl);
+
+
+
+
+    gl.drawArrays(gl.POINTS, 0, n);
+
+
 }
-draw_attribute()
+buffer_object()
 
-// 不知道有没有用
-//早期软件渲染的时代，图形绘制的确是会去判断当前界面哪些像素区域可以复用，然后优先绘制这部分内容，下一次绘制的时候就不必清空它了，这样就能达到优化的目的，但是这样的优化会导致一些游戏中常见的著名的“镜子大厅Hall of mirrors”的效应。在现代的图形绘制 这种优化已经不再有效了，最新的gpu以不同的方式工作，如果屏幕是清空状态，反而能工作的更快，**通过gpu清空屏幕 ，可以节省帧拷贝浪费的时间。因为gpu的工作方式，清空屏幕可以帮助避免很多问题，如 闪烁，或者有物品没有绘制。**
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 流程:
+// 1. 创建缓冲区对象 `gl.createBuffer()`
+// 2. 绑定缓冲区对象 `gl.bindBuffer()`
+// 3. 将数据写入缓冲区对象 `gl.bufferData()`
+// 4. 将缓冲区对象分配给一个 attribute 对象 `gl.vertexAttribPointer()`
+// 5. 开启 attribute 变量 `gl.enableVertexAttribArray()`
