686ec2da6c851567fcc63073336e0694ea01fa39
bajiu <bajiu@meishubao.com>
2021-05-18 22:10:13 +0800

    变换

--- webGL/main.js
+++ webGL/main.js
@@ -1,45 +1,24 @@
 /**
- *  2021/05/14
- *  WebGL 移动, 旋转, 和缩放
+ *  2021/05/18
+ *  变换矩阵
  */
 
+// todo
+const initVertexBuffers = (gl) => {
 
-// 变换(transformations)
-// 仿射变换(affine transformations)
-
-
-
-// 顶点着色器需要的数据，可以通过以下三种方式获得
-// - `Attributes` 属性 (从缓冲中获取的数据)
-// - `Uniforms` 全局变量 (在一次绘制中对所有顶点保持一致值)
-// - `Textures` 纹理 (从像素或纹理元素中获取的数据)
+};
 
 
+const draw_matrix = () => {
 
-
-const draw_transform = () => {
-
-    // 先画一个三角形
     const VSHADER_SOURCE =
         `
             attribute vec4 a_Position;
-            // 平移
-            uniform vec4 u_Translation; 
-            // 旋转
-            uniform float u_CosB, u_SinB;
-          
+            // 变换矩阵
+            uniform mat4 u_xformMatrix;
+            
             void main() {
-                
-                // 旋转 (具体计算参考 书第 92 页)
-                gl_Position.x = a_Position.x * u_CosB - a_Position.y * u_SinB + u_Translation.x;
-                gl_Position.y = a_Position.x * u_SinB + a_Position.y * u_CosB + u_Translation.y; 
-                   
-                // 这俩要是没有的话，就是默认值 0 了
-                gl_Position.z = a_Position.z;
-                gl_Position.w = 1.0;
-                // 平移
-                // gl_Position = a_Position + u_Translation;
-                
+                gl_Position = u_xformMatrix * a_Position;
                 gl_PointSize = 10.0;
             }
         `
@@ -50,57 +29,61 @@ const draw_transform = () => {
             }
         `
 
+    const ANGLE = 90.0;
 
+    const n = 3;
     initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+
 
     const vertices = new Float32Array([0, 0.5, -0.5, -0.5, 0.5, -0.5]);
     const vertexBuffer = gl.createBuffer();
-    const n = 3;
-
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
 
 
-    // 平移
-    // 获取平移距离地址给到 JS 变量(注意这里变成 `uniform` 了)
-    const u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
-    gl.uniform4f(u_Translation, 0.3, 0.3, 0.0, 0.0);
+    // 获取弧度
+    const radian = Math.PI * ANGLE / 180;
+    const cosB = Math.cos(radian),
+        sinB = Math.sin(radian);
 
 
-    // 旋转
-    // 为了描述一个旋转, 必须指明:
-    // - 旋转轴 (图形将围绕旋转轴旋转)
-    // - 旋转防线 (方向: 顺时针或逆时针)
-    // - 旋转角度 (图形旋转经过的角度)
+    // 注意 WebGL 中矩阵是列主序的
+    // const xformMatrix = new Float32Array([
+    //     cosB, sinB, 0.0, 0.0,
+    //     -sinB,cosB, 0.0, 0.0,
+    //     0.0,  0.0,  1.0, 0.0,
+    //     0.0,  0.0,  0.0, 1.0
+    // ])
 
-    // > 确认旋转方向(右手旋转法则): 右手握拳, 大拇指指向旋转轴正方向, 右手其余几个手指表明旋转方向
+    // 这个是平移
+    const xformMatrix = new Float32Array([
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        0.3, 0.3, 0.0, 1.0
+    ])
+    // 矩阵的主序: **按行主序(row major oder)** 和 **按列主序(column major order)**
 
-    // 旋转角度
-    const angle = 45.0;
-    // 将旋转所需的数据传递给顶点着色器
-    const radian = Math.PI * angle/180; // 转换为弧度制
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position)
 
-
-    // **这两个玩意必须接受的是弧度制!!!**(不是角度)
-    const cosB = Math.cos(radian);
-    const sinB = Math.sin(radian);
-
-    const u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
-    const u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
-
-    gl.uniform1f(u_CosB, cosB);
-    gl.uniform1f(u_SinB, sinB);
-
-
+    // 将旋转矩阵传输给顶点着色器
+    const u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
 
 
 
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0)
-    gl.enableVertexAttribArray(a_Position);
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix);
+    // gl.uniformMatrix4fv(location, transpose, array)
+    // 将 array 表示的 4 x 4 矩阵分配给由 location 指定的 uniform 变量
+    // 参数:
+    // - `location`: uniform 变量的储存位置
+    // - `transpose`: 在 WebGL中必须指定为false
+    // - `array`: 待传输的类型化数组, 4 x 4 矩阵按列主序存储在其中
+
+
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLES, 0, n);
-
-
 }
-draw_transform()
+draw_matrix()
---
+++ webGL/utils/initVertexBuffers.js
@@ -0,0 +1,9 @@
+/**
+ * initVertexBuffers 三角形矩阵练习
+ * @param gl
+ */
+
+
+const initVertexBuffers = (gl) => {
+
+}
