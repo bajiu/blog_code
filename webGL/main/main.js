cbefe58fffb7fac672037366a9c28334571ee6d8
bajiu <bajiu@meishubao.com>
2021-05-30 15:09:07 +0800

    临时提交

--- webGL/main/main.js
+++ webGL/main/main.js
@@ -10,8 +10,27 @@ const multiTexture = () => {
     const VSHADER =
         `
             attribute vec4 a_Position;
-            attribute vec2 a_Samp
+            attribute vec2 a_Texture;
+            varying vec2 v_Texture;
+            void main() {
+                gl_Position = a_Position;
+                v_Texture = a_Texture;
+            }
         `
+    const FSHADER =
+        `
+            precision mediump float;
+            uniform sampler2D u_Sampler0;
+            uniform sampler2D u_Sampler1;
+            void main() {
+                vec4 color0 = texture2D(u_Sampler0, v_Texture);
+                vec4 color1 = texture2D(u_Sampler1, v_Texture);
+                gl_FragColor = color0 * color1;
+            }
+        `
+
+    initShaders(gl, VSHADER, FSHADER);
+
 
 
 
